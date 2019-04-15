4e1d6ddde6bf8041778424f0b1ab9d46f3014dbe
RutviShah2008 <200390373@student.georgianc.on.ca>
2019-04-14 23:54:35 -0400

    Some changes in layout

--- main.js
+++ main.js
@@ -6,6 +6,8 @@
     this.equation = document.querySelector((options.equationsSelector || '#equations'));
     this.scoreResult = 0;
     this.result = 0;
+    this.equationData = [];
+    this.userData = [];
     this.evlButtonEle = document.querySelector("#evlBtn");
     this.skipButtonEle = document.querySelector("#skipBtn");
     this.quitButtonEle = document.querySelector("#quitBtn");
@@ -14,7 +16,7 @@
     this.equationsEle = document.querySelector("#equations");
     this.score = document.querySelector("#score");
     this.userInput = document.querySelector("#ansInput");
-    this.name = document.querySelector('#username').value;
+    this.name = document.querySelector('#username');
 
     //Start of the game
     if(this.generateRandomEquation) this.beginGame();
@@ -26,6 +28,8 @@
     if(this.quit) quitBtn.onclick=this.quit.bind(this);
   }
 
+
+  //localStorage.setItem('Player',JSON.stringify(obj));
   //beginFunction which will store the Name of the player and start the game.
   RandomEquationGame.prototype.beginGame=function(){
     if(this.name === ""){
@@ -34,19 +38,29 @@
     else{
       this.name=this.name;
     }
-    localStorage.setItem('name', this.name);
+    //localStorage.setItem('name', this.name);
 
     this.generateRandomEquation();
 
   }
 
+  RandomEquationGame.prototype.storeValue = function() {
+    const obj =  {
+      "Username" : this.name.value,
+      "Score" : this.scoreResult,
+      "Equation": this.equationData,
+      "UserAnswer": this.userData,
+    };
+
+    localStorage.setItem("UserData",JSON.stringify(obj));
+  };
   //Function which will create the randomEquation.
   RandomEquationGame.prototype.generateRandomEquation = function() {
-    console.log("generate equation call");
+    //console.log("generate equation call");
     var x = this.randomNumber(1,100);
     var y = this.randomNumber(1,100);
     var operator = this.randomNumber(0, 3);
-    console.log("x & y", x, y);
+    //console.log("x & y", x, y);
 
     switch (operator)
     {
@@ -72,6 +86,8 @@
             break;
     }
     this.equationsEle.textContent = this.equationString;
+    //localStorage.setItem('equation', this.equationString);
+
   };
   RandomEquationGame.prototype.randomNumber = function(min,max)
   {
@@ -95,46 +111,61 @@
     return result;
   };
 
-  RandomEquationGame.prototype.division = function(x,y) {
-    let result = x / y;
-    return result;
+  RandomEquationGame.prototype.division = function(x,y){
+  if(y==0)
+  {
+    let result = 0;
+      return result;
+  }
+  else {
+    let result = Math.round(((x / y) *100)/100);
+  return result;
+  }
+
+
   };
   RandomEquationGame.prototype.evaluateResult = function() {
-    console.log(this);
-    console.log(Number(this.userInput.value), this.result);
+    // console.log(this);
+    // console.log(Number(this.userInput.value), this.result);
     if(Number(this.userInput.value) === this.result)
     {
       this.win();
       this.generateRandomEquation();
-      localStorage.setItem('score',this.scoreResult);
+      this.userData.push(this.userInput.value);
+      //localStorage.setItem('score',this.scoreResult);
       this.userInput.value=0;
+
+      this.storeValue();
+
     }
     else {
       this.lose();
       this.generateRandomEquation();
-      localStorage.setItem('score',this.scoreResult);
+      this.userData.push(this.userInput.value);
+      //localStorage.setItem('score',this.scoreResult);
       this.userInput.value=0;
+
+      this.storeValue();
     }
   };
   RandomEquationGame.prototype.win = function() {
-    console.log("Win call");
+  //  console.log("Win call");
     this.scoreResult += 100;
     this.score.textContent = this.scoreResult;
     this.message.textContent = `Your moment in the sun!!!!  Let's play one more..Try this new one`;
+    this.equationData.push(this.equationString);
     return this.message;
     return this.scoreResult;
 
   };
   RandomEquationGame.prototype.lose = function() {
-    console.log("Lose call");
+    //console.log("Lose call");
     this.scoreResult -= 100;
     this.score.textContent = this.scoreResult;
     message.textContent = `\n Let's play again...Good Luck:)`;
+    this.equationData.push(this.equationString);
     return this.scoreResult;
-
   };
-
-
   RandomEquationGame.prototype.skip=function(){
     this.message.textContent = "";
     this.generateRandomEquation();
@@ -145,7 +176,8 @@
     localStorage.removeItem('score');
     this.score.textContent=0;
     this.message.textContent = "";
-    this.name.value = "";
+    this.userInput.value=0;
+    this.name.value = '';
   }
 
   RandomEquationGame.prototype.quit=function(){
@@ -154,9 +186,9 @@
     this.message.textContent ="";
     this.userInput.value = 0;
     this.equationsEle.textContent = "";
-    this.name.value = "";
+    this.name.value='';
     localStorage.clear();
-    //window.close();
+
   }
   document.querySelector("#btn-generate").onclick = function (){
     new RandomEquationGame();
